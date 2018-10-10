f72d39068a732cfad27a32a90afa768a65811ea9
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2018-10-10 16:57:40 -0400

    WIP on QueryDialogSystem

---
+++ Ministrare/Assets/Scripts/QueryDialogProto/QueryDialogRunner.cs
@@ -0,0 +1,84 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
+//using UnityEngine.Events;
+
+//// Runs through a dialog script
+//// This class should really be split into into two eventually
+//// One that handles loading the files, "running through it" and doing DialogNodeActions, and another that handles setting the UI shit
+//public class QueryDialogRunner : MonoBehaviour {
+//    // Stores our current node
+//    DialogNode currentDialogNode;
+
+//    // UI Elements
+//    public TextMeshProUGUI dialogDisplay;
+//    public UIDialogButton[] choiceButtons;
+
+//    // Dialog tree our runner is running
+//    DialogQuery query;
+
+//    // Test to update the GUI
+//    public UnityEvent updateGUI;
+
+//    // Use this for initialization
+//    void Start () {
+//        //Created hardcoded query data
+//        query = new DialogQuery();
+//        query.topics.Add(new DialogQueryTopic("testtopic"));
+//        query.topics[0].conversations.Add(new TreeWithId("testconvo"));
+//        query.topics[0].GetDialogTreeForId("testconvo").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo'", null, null));
+//        query.topics[0].conversations.Add(new TreeWithId("testconvo2"));
+//        query.topics[0].GetDialogTreeForId("testconvo2").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo2", null, null));
+
+
+
+//        SetCurrentNode(0);
+//    }
+
+//    void SetCurrentNode(int newNodeIndex)
+//    {
+//        // Load Next Node
+//        currentDialogNode = dialogTree.dialogNodes[newNodeIndex];
+//        // Set dialog text
+//        dialogDisplay.text = currentDialogNode.dialogText;
+//        // Execute dialog node actions
+//        // Refresh buttons ui
+//        for (int i = 0; i < choiceButtons.Length; i++)
+//        {
+//            choiceButtons[i].gameObject.SetActive(false);
+//        }
+//        for (int i = 0; i < currentDialogNode.nextNodes.Length; i++)
+//        {
+//            choiceButtons[i].gameObject.SetActive(true);
+//            choiceButtons[i].text.text = currentDialogNode.nextNodes[i].displayText;
+//        }
+
+//        if (currentDialogNode.actions != null)
+//        {
+//            for (int i = 0; i < currentDialogNode.actions.Length; i++)
+//            {
+//                // Need a more elegant way of getting the type
+//                if(currentDialogNode.actions[i].GetType() == typeof(ModifyStatsDialogNodeAction))
+//                {
+//                    var statAction = currentDialogNode.actions[i] as ModifyStatsDialogNodeAction;
+//                    // Need a more elegant way of doing this than an if statement for evey stat
+//                    if (statAction.statName == "Fear")
+//                    {
+//                        playerStats.runtimeFear += statAction.statChangeValue;
+//                    }
+//                    if (statAction.statName == "Inteligence")
+//                    {
+//                        playerStats.runtimeInteligence += statAction.statChangeValue;
+//                    }
+//                }
+//            }
+//        }
+//        updateGUI.Invoke();
+//    }
+
+//    void GoToNextNode(int nextNodeIndex)
+//    {
+//        SetCurrentNode(currentDialogNode.nextNodes[nextNodeIndex].id);
+//    }
+//}
---
+++ Ministrare/Assets/Scripts/QueryDialogProto/QueryTopic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Contains a list of DialogTrees for a given topic
+// this stores all the dialogs for "what is your RELATIONSHIP with another leader" stuff
+[System.Serializable]
+class DialogQueryTopic
+{
+    public string identifier;
+    public List<TreeWithId> conversations;
+
+    public DialogTree GetDialogTreeForId(string id)
+    {
+        foreach (TreeWithId convo in conversations)
+        {
+            if (convo.identifier == id)
+            {
+                return convo.dialogTree;
+            }
+        }
+        return null;
+    }
+
+    public DialogQueryTopic(string p_id)
+    {
+        identifier = p_id;
+        conversations = new List<TreeWithId>();
+    }
+}
+
+// Gives each dialogTree an id to identify itself by in a QueryTopic
+[System.Serializable]
+class TreeWithId
+{
+    public string identifier;
+    public DialogTree dialogTree;
+
+    public TreeWithId(string p_id)
+    {
+        dialogTree = new DialogTree();
+        identifier = p_id;
+    }
+}
