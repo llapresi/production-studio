a66782b084d0b5d64bc0132a3adae2df0bcc1f4d
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2018-10-10 17:28:01 -0400

    added hacky test of query dialog for Milestone 1 roundtable. When we get to the actual implementation a lot of this is gonna change so I don't necessarily want to merge this into master yet at this state.

---
+++ Ministrare/Assets/Scripts/ButtonScripts/QueryTestButtonManager.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QueryTestButtonManager : MonoBehaviour {
+
+    public enum buttonState {basic,select,done};
+
+    private string[] opOne;
+    private string[] opTwo;
+    private string[] opThree;
+    private string[] opFour;
+
+    private string[][] ops;
+
+    public GameObject buttonPrefab;
+
+    public GameObject[] buttonList;
+
+    string[] chosen;
+
+    string firstEntry;
+    string secondEntry;
+
+    private Vector3 pos = new Vector3(200,400,0);
+
+
+    private int arrayPlace;
+
+    public buttonState state;
+
+    // Reference to our Query Dialog Runner
+    public QueryDialogRunner queryRunner;
+
+	// Use this for initialization
+	void Start () {
+        state = buttonState.basic;
+        opOne = new string[] { "test convo", "test convo 2"};
+        opTwo = new string[] { "test1", "test2", "test3"};
+        opThree = new string[] { "test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10", "test11", "test12"};
+        opFour = new string[] { "test1", "test2", "test3", "test4", "test5", "test6", "test7" };
+        initialized = false;
+        ops = new string[][] { opOne, opTwo, opThree, opFour };
+    }
+
+    public bool initialized;
+    bool cleared;
+    bool selected;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        switch (state)
+        {
+            case buttonState.basic:
+                Init();
+                break;
+
+            case buttonState.select:
+                Selection();
+                break;
+
+            case buttonState.done:
+                Clear();
+                break;
+        }
+
+
+	}
+
+    /// <summary>
+    /// Places initial buttons and sets their attributes.  Only runs if not already initialized
+    /// </summary>
+    void Init()
+    {
+        if (initialized == false)
+        {
+            arrayPlace = 0;
+            buttonList = new GameObject[4];
+            for(int x = 0; x < 1; x++)
+            {
+                GameObject ob = CreateButton("test topic", ops[x]);
+                buttonList[x] = ob;
+                if (x % 2 == 0)
+                {
+                    pos.x += 200;
+                }
+                else
+                {
+                    pos.y -= 100;
+                    pos.x = 200;
+                }
+            }
+            initialized = true;
+        }
+    }
+
+
+    /// <summary>
+    /// Places slection buttons.  Only if not already placed
+    /// </summary>
+    void Selection()
+    {
+        if(selected == false)
+        {
+            pos.x = 200;
+            pos.y = 500;
+            selected = true;
+            buttonList = new GameObject[chosen.Length];
+            arrayPlace = 0;
+            foreach (string entry in chosen)
+            {
+                createSelectionButton(entry);
+              
+            }
+        }
+    }
+
+    /// <summary>
+    /// Clears all buttons from screen.  only runs if not already cleared
+    /// </summary>
+    void Clear()
+    {
+        foreach (GameObject button in buttonList)
+        {
+            Destroy(button);
+
+        }
+    }
+
+    /// <summary>
+    /// creates a button with the given string as its text.  Used for initial buttons
+    /// </summary>
+    /// <param name="name"></param>
+    GameObject CreateButton(string name, string[] op)
+    {
+        GameObject initButton = Instantiate(buttonPrefab);
+
+        initButton.transform.SetParent(gameObject.transform);
+
+        var button = initButton.GetComponent<UnityEngine.UI.Button>();
+
+        button.onClick.AddListener(() => HandleClickBasic(op));
+
+        initButton.GetComponentInChildren<Text>().text = name;
+
+        initButton.transform.position = pos;
+
+        buttonList[arrayPlace] = initButton;
+        arrayPlace++;
+
+        return initButton;
+    }
+
+
+    /// <summary>
+    /// Creates a button with the given string as its text.  used for secondary buttons
+    /// </summary>
+    /// <param name="name"></param>
+    GameObject createSelectionButton(string name)
+    {
+        GameObject selectButton = Instantiate(buttonPrefab);
+
+        selectButton.transform.SetParent(gameObject.transform);
+
+        var button = selectButton.GetComponent<UnityEngine.UI.Button>();
+
+        button.onClick.AddListener(() => MS1SelectConvo(name));
+
+        selectButton.GetComponentInChildren<Text>().text = name;
+
+        selectButton.transform.position = pos;
+
+        configureSelectPosition();
+
+        buttonList[arrayPlace] = selectButton;
+        arrayPlace++;
+
+        return selectButton;
+    }
+
+    void MS1SelectConvo(string convoName)
+    {
+        queryRunner.GoToTestConvoWithName(convoName);
+    }
+
+    /// <summary>
+    /// Assigns click behavior for initial buttons
+    /// </summary>
+    void HandleClickBasic(string[] op)
+    {
+        Debug.Log("click");
+        state = buttonState.select;
+        chosen = op;
+        foreach (GameObject button in buttonList)
+        {
+            Destroy(button);
+
+        }
+    }
+
+
+    /// <summary>
+    /// Assings click behavior for secondary buttons
+    /// </summary>
+    void HandleClickSelect()
+    {
+
+    }
+
+    void configureSelectPosition()
+    {
+        int offset = 0;
+        int posIncrease = 0;
+        if(chosen.Length - arrayPlace > 5)
+        {
+            if((arrayPlace + 1) % 5 == 0)
+            {
+                pos.y -= 100;
+                pos.x = 200;
+            }
+            else
+            {
+                pos.x += 200;
+            }
+        }
+        else
+        {
+            if ((arrayPlace + 1) % 5 == 0)
+            {
+                pos.y -= 100;
+                pos.x = 200;
+            }
+            if (offset != 0)
+            {
+                offset = 200 / (chosen.Length - arrayPlace);
+                pos.x = 200 + offset;
+            }
+            pos.x += 200;
+        }
+    }
+}
--- Ministrare/Assets/Scripts/QueryDialogProto/QueryDialogRunner.cs
+++ Ministrare/Assets/Scripts/QueryDialogProto/QueryDialogRunner.cs
@@ -1,84 +1,51 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using TMPro;
-//using UnityEngine.Events;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
 
-//// Runs through a dialog script
-//// This class should really be split into into two eventually
-//// One that handles loading the files, "running through it" and doing DialogNodeActions, and another that handles setting the UI shit
-//public class QueryDialogRunner : MonoBehaviour {
-//    // Stores our current node
-//    DialogNode currentDialogNode;
+// Runs through a dialog script
+// This class should really be split into into two eventually
+// One that handles loading the files, "running through it" and doing DialogNodeActions, and another that handles setting the UI shit
+public class QueryDialogRunner : MonoBehaviour
+{
+    // Stores our current node
+    DialogNode currentDialogNode;
 
-//    // UI Elements
-//    public TextMeshProUGUI dialogDisplay;
-//    public UIDialogButton[] choiceButtons;
+    // UI Elements
+    public TextMeshProUGUI dialogDisplay;
+    public UIDialogButton[] choiceButtons;
 
-//    // Dialog tree our runner is running
-//    DialogQuery query;
+    // Dialog tree our runner is running
+    DialogQuery query;
 
-//    // Test to update the GUI
-//    public UnityEvent updateGUI;
+    // Test to update the GUI
+    public UnityEvent updateGUI;
 
-//    // Use this for initialization
-//    void Start () {
-//        //Created hardcoded query data
-//        query = new DialogQuery();
-//        query.topics.Add(new DialogQueryTopic("testtopic"));
-//        query.topics[0].conversations.Add(new TreeWithId("testconvo"));
-//        query.topics[0].GetDialogTreeForId("testconvo").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo'", null, null));
-//        query.topics[0].conversations.Add(new TreeWithId("testconvo2"));
-//        query.topics[0].GetDialogTreeForId("testconvo2").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo2", null, null));
+    // Use this for initialization
+    void Start()
+    {
+        //Created hardcoded query data
+        query = new DialogQuery();
+        query.topics.Add(new DialogQueryTopic("test subject"));
+        query.topics[0].conversations.Add(new TreeWithId("test convo"));
+        query.topics[0].GetDialogTreeForId("test convo").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo'", null, null));
+        query.topics[0].conversations.Add(new TreeWithId("test convo 2"));
+        query.topics[0].GetDialogTreeForId("test convo 2").dialogNodes.Add(new DialogNode("This is a another test response for conversation 'testconvo2", null, null));
+    }
 
+    void SetCurrentNode(DialogNode newDialogNode)
+    {
+        currentDialogNode = newDialogNode;
+        dialogDisplay.text = currentDialogNode.dialogText;
+        updateGUI.Invoke();
+    }
 
+    // Bad hardcoded functions
+    // Using this for milestone 1 showcase
 
-//        SetCurrentNode(0);
-//    }
-
-//    void SetCurrentNode(int newNodeIndex)
-//    {
-//        // Load Next Node
-//        currentDialogNode = dialogTree.dialogNodes[newNodeIndex];
-//        // Set dialog text
-//        dialogDisplay.text = currentDialogNode.dialogText;
-//        // Execute dialog node actions
-//        // Refresh buttons ui
-//        for (int i = 0; i < choiceButtons.Length; i++)
-//        {
-//            choiceButtons[i].gameObject.SetActive(false);
-//        }
-//        for (int i = 0; i < currentDialogNode.nextNodes.Length; i++)
-//        {
-//            choiceButtons[i].gameObject.SetActive(true);
-//            choiceButtons[i].text.text = currentDialogNode.nextNodes[i].displayText;
-//        }
-
-//        if (currentDialogNode.actions != null)
-//        {
-//            for (int i = 0; i < currentDialogNode.actions.Length; i++)
-//            {
-//                // Need a more elegant way of getting the type
-//                if(currentDialogNode.actions[i].GetType() == typeof(ModifyStatsDialogNodeAction))
-//                {
-//                    var statAction = currentDialogNode.actions[i] as ModifyStatsDialogNodeAction;
-//                    // Need a more elegant way of doing this than an if statement for evey stat
-//                    if (statAction.statName == "Fear")
-//                    {
-//                        playerStats.runtimeFear += statAction.statChangeValue;
-//                    }
-//                    if (statAction.statName == "Inteligence")
-//                    {
-//                        playerStats.runtimeInteligence += statAction.statChangeValue;
-//                    }
-//                }
-//            }
-//        }
-//        updateGUI.Invoke();
-//    }
-
-//    void GoToNextNode(int nextNodeIndex)
-//    {
-//        SetCurrentNode(currentDialogNode.nextNodes[nextNodeIndex].id);
-//    }
-//}
+    public void GoToTestConvoWithName(string name)
+    {
+        SetCurrentNode(query.topics[0].GetDialogTreeForId(name).dialogNodes[0]);
+    }
+}
