a9eac794679ba1959d1e7eceb9e217e8521f7de7
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2018-10-15 17:18:28 -0400

    QueryDialogRunner now reads from JSON file. NOTE: We still need to implement different dialog nodes based on the percentage of a variable for a Leader's stat

--- Ministrare/Assets/Scripts/QueryDialogProto/QueryDialogRunner.cs
+++ Ministrare/Assets/Scripts/QueryDialogProto/QueryDialogRunner.cs
@@ -24,23 +24,14 @@ public class QueryDialogRunner : MonoBehaviour
 
     public UIQueryButtonGroup buttonGroup;
 
+    public string filepath;
+
     // Use this for initialization
     void Start()
     {
-        //Created hardcoded query data
-        currentQuery = new DialogQuery();
-        currentQuery.topics.Add(new DialogQueryTopic("test topic"));
-        currentQuery.topics[0].conversations.Add(new DialogTreeWithId("test convo"));
-        currentQuery.topics[0].GetDialogTreeForId("test convo").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo'", null, null));
-        currentQuery.topics[0].conversations.Add(new DialogTreeWithId("test convo 2"));
-        currentQuery.topics[0].GetDialogTreeForId("test convo 2").dialogNodes.Add(new DialogNode("This is a another test response for conversation 'testconvo 2", null, null));
-        currentQuery.topics.Add(new DialogQueryTopic("another test topic"));
-        currentQuery.topics[1].conversations.Add(new DialogTreeWithId("test convo 3"));
-        currentQuery.topics[1].GetDialogTreeForId("test convo 3").dialogNodes.Add(new DialogNode("This is a test response for conversation 'testconvo 3'", null, null));
-        currentQuery.topics[1].conversations.Add(new DialogTreeWithId("test convo 4"));
-        currentQuery.topics[1].GetDialogTreeForId("test convo 4").dialogNodes.Add(new DialogNode("This is a another test response for conversation 'testconvo 4", null, null));
-
-        JsonUtility.ToJson(currentQuery);
+        // Load our test JSON dialogQuery
+        TextAsset targetFile = Resources.Load<TextAsset>(filepath);
+        currentQuery = JsonUtility.FromJson<DialogQuery>(targetFile.text);
 
         buttonGroup.rootQuery = currentQuery;
         buttonGroup.CreateRootButtons();
