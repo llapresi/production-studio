00a65710e5332a7f3cc3c55491bd21f832e1441a
Johnny Siu <jxs7205@rit.edu>
2018-10-15 19:47:30 -0400

    Added into ResourceManager attributes to calculate Happiness of the specific group and Military gains

--- Ministrare/Assets/Scripts/JohnnyTestScene/ResourceManager.cs
+++ Ministrare/Assets/Scripts/JohnnyTestScene/ResourceManager.cs
@@ -16,6 +16,10 @@ public class ResourceManager : ScriptableObject {
     private int initFoodUpkeep = 30;
     [SerializeField]
     private int initFoodProduction = 20;
+    [SerializeField]
+    private double initFoodHappinessEfficiency = 1;
+    [SerializeField]
+    private int initFoodMilitaryGained = 0;
     //Gold 
     [SerializeField]
     private int initGoldStorage = 100;
@@ -23,6 +27,10 @@ public class ResourceManager : ScriptableObject {
     private int initGoldUpkeep = 20;
     [SerializeField]
     private int initGoldProduction = 30;
+    [SerializeField]
+    private double initGoldHappinessEfficiency = 1;
+    [SerializeField]
+    private int initGoldMilitaryGained = 0;
     //Exotic Goods
     [SerializeField]
     private int initEGStorage = 100;
@@ -30,20 +38,30 @@ public class ResourceManager : ScriptableObject {
     private int initEGUpkeep = 10;
     [SerializeField]
     private int initEGProduction = 40;
+    [SerializeField]
+    private double initEGHappinessEfficiency = 1;
+    [SerializeField]
+    private int initEGMilitaryGained = 0;
 
     //values in game 
     //Food
     public int runtimeFoodStorage;
     public int runtimeFoodUpkeep;
     public int runtimeFoodProduction;
+    public double runtimeFoodHappinessEfficiency;
+    public int runtimeFoodMiliaryGained;
     //Gold
     public int runtimeGoldStorage;
     public int runtimeGoldUpkeep;
     public int runtimeGoldProduction;
+    public double runtimeGoldHappinessEfficiency;
+    public int runtimeGoldMiliaryGained;
     //EG
     public int runtimeEGStorage;
     public int runtimeEGUpkeep;
     public int runtimeEGProduction;
+    public double runtimeEGHappinessEfficiency;
+    public int runtimeEGMiliaryGained;
 
     public void OnEnable()
     {
@@ -52,22 +70,33 @@ public class ResourceManager : ScriptableObject {
         runtimeFoodStorage = initFoodStorage;
         runtimeFoodUpkeep = initFoodUpkeep;
         runtimeFoodProduction = initFoodProduction;
+        runtimeFoodHappinessEfficiency = initFoodHappinessEfficiency;
+        runtimeFoodMiliaryGained = initFoodMilitaryGained;
         //Gold
         runtimeGoldStorage = initGoldStorage;
         runtimeGoldUpkeep = initGoldUpkeep;
         runtimeGoldProduction = initGoldProduction;
+        runtimeGoldHappinessEfficiency = initGoldHappinessEfficiency;
+        runtimeGoldMiliaryGained = initGoldMilitaryGained;
         //Exotic Goods
         runtimeEGStorage = initEGStorage;
         runtimeEGUpkeep = initEGUpkeep;
         runtimeEGProduction = initEGProduction;
+        runtimeEGHappinessEfficiency = initEGHappinessEfficiency;
+        runtimeEGMiliaryGained = initEGMilitaryGained;
     }
 
     // computes how much of each resource remains after a day
     public void processResource()
     {
-        runtimeFoodStorage = runtimeFoodStorage + runtimeFoodProduction - runtimeFoodUpkeep;
-        runtimeGoldStorage = runtimeGoldStorage + runtimeGoldProduction - runtimeGoldUpkeep;
-        runtimeEGStorage = runtimeEGStorage + runtimeEGProduction - runtimeEGUpkeep;
+        // calculate total food, gold and EG produced
+        int totalFoodProduced = (int)(runtimeFoodHappinessEfficiency * runtimeFoodProduction + runtimeFoodMiliaryGained);
+        int totalGoldProduced = (int)(runtimeGoldHappinessEfficiency * runtimeGoldProduction + runtimeGoldMiliaryGained);
+        int totalEGProduced = (int)(runtimeEGHappinessEfficiency * runtimeEGProduction + runtimeEGMiliaryGained);
+        // Factor in the production and upkeep to get new storage amounts 
+        runtimeFoodStorage = runtimeFoodStorage + totalFoodProduced - runtimeFoodUpkeep;
+        runtimeGoldStorage = runtimeGoldStorage + totalGoldProduced - runtimeGoldUpkeep;
+        runtimeEGStorage = runtimeEGStorage + totalEGProduced - runtimeEGUpkeep;
     }
 
 }
