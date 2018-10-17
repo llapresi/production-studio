3507ba014451e701296745223af271b20b7728a6
MAIN\ejp4604 <ejp4604@rit.edu>
2018-10-17 18:41:12 -0400

    Base tech tree structure

---
+++ Ministrare/Assets/Scripts/ElliotScripts/TechTree.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New TechTree", menuName = "Ministrare/SingletonVars/TechTree", order = 1)]
+public class TechTree : ScriptableObject {
+
+    [Header("Initial Values")]
+    [SerializeField]
+    private TechNode[] initialNodes;
+    public TechNode[] runtimeNodes;
+    
+    public void OnEnable()
+    {
+        this.hideFlags = HideFlags.DontUnloadUnusedAsset;
+        runtimeNodes = initialNodes;
+    }
+}
--- Ministrare/Assets/Scripts/ElliotScripts/Technology.cs
+++ Ministrare/Assets/Scripts/ElliotScripts/Technology.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Technology : MonoBehaviour {
+[System.Serializable]
+public class TechNode {
 
     public string name;
-    public Technology nextTech1 = new Technology();
-    public Technology nextTech2 = new Technology();
-    public bool researched = false;
+    public bool researched;
+    public int startLevel;
+    public string placeHolderEnum;
     // component from resource management that allows edits to increased resources per turn (research/production/unit damage/etc...)
     // boolean allowing new struture to be built
 
