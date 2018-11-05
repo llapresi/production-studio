bbbb278010fb27f26fbb40f39314eb7aba18fc00
Jackson Wootan <JacksonWootan@gmail.com>
2018-11-05 12:57:02 -0500

    Started adding funcionality to UI
    Added screen transition funcionality to a few uttons

---
+++ Ministrare/Assets/Scripts/UIScripts/LoadChatScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    private bool paused;
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paused = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (!paused)
+            {
+                canvas.gameObject.SetActive(true);
+                paused = true;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                paused = false;
+            }
+        }
+	}
+
+    public void Load(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
---
+++ Ministrare/Assets/Scripts/UIScripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadChatScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            SceneManager.LoadScene("PauseMenuUI", LoadSceneMode.Additive);
+        }
+	}
+
+    public void Load(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
