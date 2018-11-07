267c8f5bab170de3ec17d05017b55aee26b29f62
Jackson Wootan <JacksonWootan@gmail.com>
2018-11-06 21:07:58 -0500

    Fixed Transitions for nonChat scenes

--- Ministrare/Assets/Scripts/UIScripts/UItest.cs
+++ Ministrare/Assets/Scripts/UIScripts/UItest.cs
@@ -7,18 +7,38 @@ using UnityEngine.UI;
 public class UItest : MonoBehaviour
 {
 
+    //Canvas used for pause screen
+    public Canvas pauseCanvas;
+
+
     // Use this for initialization
     void Start()
     {
-
+        if (pauseCanvas != null)
+        {
+            pauseCanvas.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("escape"))
+        if (pauseCanvas != null)
         {
-            SceneManager.LoadScene("PauseMenuUI", LoadSceneMode.Additive);
+
+
+            if (Input.GetKeyDown("escape"))
+            {
+                if (pauseCanvas.enabled)
+                {
+                    pauseCanvas.enabled = false;
+                }
+                else
+                {
+                    pauseCanvas.enabled = true;
+                }
+            }
+
         }
     }
 
@@ -27,6 +47,15 @@ public class UItest : MonoBehaviour
         SceneManager.LoadScene(level, LoadSceneMode.Additive);
     }
 
+    //loads scene nonadditively
+    public void NewScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
 
+    public void UnLoad(GameObject root)
+    {
+        Destroy(root);
+    }
 
 }
