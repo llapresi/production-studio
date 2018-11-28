e01917ae058a8d9d28f709158e3bdac9426e7a1b
Jackson Wootan (RIT Student) <jfw6252@ad.rit.edu>
2018-11-28 15:38:43 -0500

    Map Panning
    added ability to drag map.  Still need to add zoom and bounds.

---
+++ Ministrare/Assets/Scripts/UIScripts/moveMap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class moveMap : MonoBehaviour, IDragHandler {
+
+    private float xloc = 0;
+    private float yloc = 0;
+    private float previousXLoc = 0;
+    private float previousYLoc = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateOffset();
+
+	}
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("test");
+       
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + (xloc - previousXLoc), gameObject.transform.position.y + (yloc - previousYLoc), gameObject.transform.position.z);
+            
+    }
+
+    public void UpdateOffset()
+    {
+        previousXLoc = xloc;
+        previousYLoc = yloc;
+        xloc = Input.mousePosition.x;
+        yloc = Input.mousePosition.y;
+    }
+}
