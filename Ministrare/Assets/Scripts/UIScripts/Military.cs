665e5ae8f7d03ac4f0a4bbc4001088f166b6936e
Jackson Wootan <JacksonWootan@gmail.com>
2018-12-02 23:34:52 -0500

    Military Updates

--- Ministrare/Assets/Scripts/UIScripts/Military.cs
+++ Ministrare/Assets/Scripts/UIScripts/Military.cs
@@ -13,23 +13,92 @@ public struct Unit
     public int shield;
     //amount of damage they can take before death
     public int health;
+    public int healthMax;
 
     //location of unit
-    public int xLoc;
-    public int yLoc;
+    public float xLoc;
+    public float yLoc;
+
+    //speed value for moving across map
+    private int speed;
+
+    public int IFF;
+
+    public GameObject image;
+
 
     //where they are trying to go/attack
     public GameObject objective;
 
     //constructor
-    public Unit(int a, int s, int h, int x, int y, GameObject obj)
+    public Unit(int a, int s, int h, float x, float y, GameObject obj, int iff, GameObject im, GameObject parent)
     {
         attack = a;
         shield = s;
         health = h;
+        healthMax = h;
         xLoc = x;
         yLoc = y;
         objective = obj;
+        speed = 10;
+        IFF = iff;
+        image = im;
+        im.transform.position = new Vector3(xLoc, yLoc, 0);
+        im.transform.parent = parent.transform;
+    }
+
+    /// <summary>
+    /// done when days roll over to move units
+    /// </summary>
+    public void Move()
+    {
+
+        float targetX = objective.transform.position.x;
+        float targetY = objective.transform.position.y;
+
+        float distanceX = Mathf.Abs(xLoc - targetX);
+        float distanceY = Mathf.Abs(yLoc - targetY);
+
+        if(distanceX < speed)
+        {
+            xLoc = targetX;
+        }
+        else
+        {
+            if(targetX < xLoc)
+            {
+                xLoc -= speed;
+            }
+            else
+            {
+                xLoc += speed;
+            }
+        }
+        if (distanceY < speed)
+        {
+            yLoc = targetY;
+        }
+        else
+        {
+            if (targetY < yLoc)
+            {
+                yLoc -= speed;
+            }
+            else
+            {
+                yLoc += speed;
+            }
+        }
+        image.transform.position = new Vector3(xLoc, yLoc, 0);
+
+    }
+
+    public void Rest()
+    {
+        if(health < healthMax)
+        {
+            health += 1;
+        }
     }
 }
 
@@ -40,11 +109,14 @@ public class Military : MonoBehaviour
     private int attack = 5;
     private int shield = 5;
     private int health = 10;
-    private int xLoc = 10;
-    private int yLoc = 10;
+    private float xLoc = 100;
+    private float yLoc = 100;
 
+    public GameObject unitImOne;
 
     private List<Unit> unitList = new List<Unit>();
+
+    private List<Unit> enemyList = new List<Unit>();
 
     private List<GameObject> locList = new List<GameObject>();
 
@@ -53,13 +125,20 @@ public class Military : MonoBehaviour
     /// <summary>
     /// creates a unit and adds them to the list
     /// </summary>
-    public void createUnit()
+    public void createUnit(int iff)
     {
-        Unit newUnit = new Unit(attack,shield,health,xLoc,yLoc, gameObject);
+        GameObject unitIm = Instantiate(unitImOne);
+        Unit newUnit = new Unit(attack,shield,health,xLoc,yLoc, gameObject, iff, unitIm, this.gameObject);
         unitList.Add(newUnit);
     }
 
-    
+    public void MoveUnits()
+    {
+        foreach (Unit toMove in unitList)
+        {
+            toMove.Move();
+        }
+    }
 
     /// <summary>
     /// distributes units to each objective
@@ -69,19 +148,53 @@ public class Military : MonoBehaviour
         int x = 0;
         for(int i = 0; i < locList.Count; i++)
         {
-            toChange = unitList[i];
-            toChange.objective = locList[x];
-            unitList[i] = toChange;
-            if(x < locList.Count)
+            if (unitList[i].IFF == 0)
             {
-                x++;
-                
-            }
-            else
-            {
-                x = 0;
+                toChange = unitList[i];
+                toChange.objective = locList[x];
+                unitList[i] = toChange;
+                if (x < locList.Count)
+                {
+                    x++;
+
+                }
+                else
+                {
+                    x = 0;
+                }
             }
         }
+    }
+
+
+    /// <summary>
+    /// if units fight, checks for nearby units to jpoin the battle
+    /// </summary>
+    /// <param name="agressor"></param>
+    /// <param name="defender"></param>
+   public void startBattle(Unit agressor, Unit defender)
+    {
+        List<Unit> liOne = new List<Unit>();
+        List<Unit> liTwo = new List<Unit>();
+        float distCheck = 100;
+
+        foreach (Unit toCheck in unitList)
+        {
+            float distance = Vector3.Distance((new Vector3(agressor.xLoc, agressor.yLoc, 0)), (new Vector3(toCheck.xLoc, toCheck.yLoc, 0)));
+            if(distance < distCheck)
+            {
+                if(agressor.IFF == toCheck.IFF)
+                {
+                    liOne.Add(toCheck);
+                }
+                else
+                {
+                    liTwo.Add(toCheck);
+                }
+            }
+        }
+
+        battle(liOne, liTwo);
     }
 
     /// <summary>
